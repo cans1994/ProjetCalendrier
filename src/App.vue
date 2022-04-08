162c415160ff682ee412d56b03b6d53c2cb57206
cans1994 <cans.bl@hotmail.com>
2022-04-08 13:23:38 +0200

    ajout du header de @ThomcodeFr

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
+import Header from "./components/Header.vue";
 import EventCalendar from "./components/EventCalendar.vue";
 </script>
 
 <template>
-  <EventCalendar />
+  <header>
+    <Header />
+  </header>
+
+  <main>
+    <EventCalendar />
+  </main>
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap");
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'Lernumerique :',
+      subtitle: 'agenda',
+      blog: 'Blog',
+      contact: 'Contact'
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="container-header">
+    <div class="left-part">
+      <h1 class="main-title">
+        {{ title }} <span class="subtitle"> {{ subtitle }}</span>
+      </h1>
+    </div>
+    <div class="right-part">
+      <h2 class="menu"> {{ blog }} • {{ contact }}</h2>
+    </div>
+  </div>
+
+  <hr />
+</template>
+
+<style>
+.container-header{
+  display: flex;
+  justify-content: space-between;
+}
+.main-title {
+  font-family: 'Roboto', Courier, monospace;
+  padding-left: 1.25rem;
+  font-weight: 800;
+}
+.subtitle {
+  color: #ae2012;
+  text-transform: uppercase;
+  font-weight: 800;
+}
+.menu {
+  text-transform: uppercase;
+  font-weight: 800;
+}
+</style>
