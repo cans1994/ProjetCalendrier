d2e48bcadcc531257bb0ec26663637e58cba56a9
cans1994 <cans.bl@hotmail.com>
2022-04-08 13:06:22 +0200

    calendrier en français, désormais sur un autre socle

---
+++ src/components/EventCalendar.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { reactive } from "vue"; // => est la capacité pour une variable (array, string, number, object, etc) de se mettre à jour lorsque sa valeur ou toute autre variable à laquelle elle fait référence est modifiée après la déclaration
+import "@fullcalendar/core/vdom"; // solves problem with Vite
+import FullCalendar from "@fullcalendar/vue3";
+import frLocale from "@fullcalendar/core/locales/fr";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+// import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
+//import useEvents from '../composables/useEvents.js'
+
+//const id = ref(10);
+
+//const ( getEvents ) = useEvents[]
+
+const options = reactive({
+  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+  initialView: "dayGridMonth",
+  locale: frLocale,
+  headerToolbar: {
+    left: "prev,next, today",
+    center: "title",
+    right: "dayGridMonth",
+  },
+  editable: true,
+  selectable: true,
+  weekends: true,
+  select: (arg) => {
+    //argument est passé à la directive
+    id.value = id.value + 1;
+
+    let str = formatDate("2018-09-01", {
+      month: "long",
+      year: "numeric",
+      day: "numeric",
+      timeZoneName: "short",
+      timeZone: "UTC+2",
+      locale: "fr",
+    });
+
+    const cal = arg.view.calendar;
+    cal.unselect();
+    cal.addEvent({
+      id: `${id.value}`,
+      title: `New event ${id.value}`,
+      start: arg.start,
+      end: arg.end,
+      allDay: true,
+    });
+  },
+  eventClick: (arg) => {
+    if (arg.event) {
+      arg.event.remove();
+    }
+  },
+  events: [],
+  eventAdd: (arg) => {
+    createEvent({
+      id: arg.event.id,
+      title: arg.event.title,
+      start: arg.event.start,
+      end: arg.event.end,
+      allDay: arg.event.allDay,
+    });
+  },
+  eventChange: (arg) => {
+    updateEvent({
+      id: arg.event.id,
+      title: arg.event.start,
+      start: arg.event.start,
+      end: arg.event.end,
+      allDay: arg.event.allDay,
+    });
+  },
+  eventRemove: (arg) => {
+    deleteEvent(arg.event.id);
+  },
+});
+
+// options.events = getEvents.value;
+// watch(getEvents, () => {
+//   options.events = getEvents.value;
+// });
+</script>
+
+<template>
+  <FullCalendar v-bind:options="options" />
+</template>
